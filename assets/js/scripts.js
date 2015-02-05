f542f8aa219169dc5caada52a70d882dc24bf43e
Gustav Lindqvist <gustav@lindqvist.io>
2015-02-05 21:03:38 +0100

    Fix feed height for archive pages with one featured post only
    Closes #46
    
    - Fix feed height for archive pages with one featured post only
    - Clean up some unused styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@
 !function(){
     var Saga = {
 
-        /* 
+        /*
          *  Switch this to true if all the dependencies are already loaded,
          *  for example via a bundeld js file.
          */
@@ -32,19 +32,19 @@
             }
 
             this.getScript('/assets/js/helper/highlight.min.js')
-            .then(function() { 
-                hljs.initHighlightingOnLoad();
-            });
+                .then(function() {
+                    hljs.initHighlightingOnLoad();
+                });
         },
 
         responsiveVideos: function(){
-            this.getScript('/assets/js/helper/jquery.fitvids.js').then($.proxy(function() { 
+            this.getScript('/assets/js/helper/jquery.fitvids.js').then($.proxy(function() {
                 this.$main.fitVids();
             }, this));
         },
 
         gallery: function(){
-            if( $('p a:not(:only-child) img').closest('p').length === 0 
+            if( $('p a:not(:only-child) img').closest('p').length === 0
                 && $('p img:not(:only-child)').closest('p').length !== 0){
                 return;
             }
@@ -59,19 +59,19 @@
 
         onGallery: function(){
             this.getScript('/assets/js/helper/gallery.min.js').then(function() { // Load in script for gallery
-                    var size = 0;
-                    if ($(window).height() > $(window).width()){
-                        size = $(window).height();
-                    } else {
-                        size = $(window).width();
-                    }
-                    if (size < 210){
-                        size = 210;
-                    }
-                    $('.gallery').removeWhitespace().collagePlus({
-                            'targetHeight': size/5
-                    });
+                var size = 0;
+                if ($(window).height() > $(window).width()){
+                    size = $(window).height();
+                } else {
+                    size = $(window).width();
+                }
+                if (size < 210){
+                    size = 210;
+                }
+                $('.gallery').removeWhitespace().collagePlus({
+                    'targetHeight': size/5
                 });
+            });
         },
 
         fullWidthImages: function(){
@@ -80,10 +80,10 @@
             }
 
             this.getScript('/assets/js/helper/imagesloaded.pkgd.min.js')
-            .then($.proxy(function() {
-                this.$main.imagesLoaded($.proxy(this.onFullWidthImages, this));
-                $(window).resize($.proxy(this.onFullWidthImages, this));
-            }, this));
+                .then($.proxy(function() {
+                    this.$main.imagesLoaded($.proxy(this.onFullWidthImages, this));
+                    $(window).resize($.proxy(this.onFullWidthImages, this));
+                }, this));
         },
 
         onFullWidthImages: function(){
@@ -121,7 +121,7 @@
 
         stickyFooter: function(){
             var resize = $.proxy(function(){
-                this.$main.css('min-height', 
+                this.$main.css('min-height',
                     $(window).height() - $('#header').height() - $('#footer').height()
                 );
             }, this);
@@ -205,7 +205,7 @@
                 $(this).html("<i class='fa fa-spinner fa-spin'></i>");
 
                 // Grab data from next page
-                $.get(this.nextLink, function(data){ 
+                $.get(this.nextLink, function(data){
                     // Append all posts to #content
                     var posts = $(data).find('.post');
                     $.each(posts,function(){
@@ -230,7 +230,7 @@
                 });
             } else {
                 $('#loadmore').remove();
-            }   
+            }
         },
 
         loadedScripts: {},
@@ -245,17 +245,17 @@
                         loader();
                     } else {
                         $.getScript(this.siteurl + path)
-                        .then($.proxy(function(){
-                            this.loadedScripts[path] = true;
-                            loader();
-                        }, this))
-                        .fail(function(err){
-                            if(err){
-                                promise.rejectWith(err);
-                            } else {
-                                promise.reject();
-                            }
-                        });
+                            .then($.proxy(function(){
+                                this.loadedScripts[path] = true;
+                                loader();
+                            }, this))
+                            .fail(function(err){
+                                if(err){
+                                    promise.rejectWith(err);
+                                } else {
+                                    promise.reject();
+                                }
+                            });
                     }
                 } else {
                     promise.resolve();
@@ -267,7 +267,7 @@
             } else {
                 loader();
             }
-            
+
             return promise;
         },
 
