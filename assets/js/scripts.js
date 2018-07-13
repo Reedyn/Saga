9d08d299fdde0cd6c6162f56b5dc697862a16cd7
Gustav Lindqvist <gustav@lindqvist.io>
2018-07-13 17:19:26 +0200

    Updated navigation using Ghost menu and with hack for icons. Also fixed menu colors based on cover images and page.
    Closes #39, #92, #86

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,21 @@
-/* globals hljs, $, console */
 /* eslint-env: jQuery */
 /* exported Saga */
 
 let Saga = (() => {
-    const five = null;
+    let nav = document.querySelector('#dropdown-menu--toggle');
+    let toggleMenu = (selector) => {
+        let element = document.querySelector(selector);
+        element.setAttribute('data-state', element.getAttribute('data-state') === 'closed' ? 'open' : 'closed');
+    };
 
-    return five;
+    nav.onclick = (e) => {
+        toggleMenu('#dropdown-menu', 'closed', 'open');
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    document.onclick = () => {
+        let element = document.querySelector('#dropdown-menu');
+        element.setAttribute('data-state', 'closed');
+    };
 })();
