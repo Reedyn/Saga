933c0610b19744b072da51785a23632b4dda0781
Gustav Lindqvist <gustav@lindqvist.io>
2018-07-14 08:29:29 +0200

    Added translations for everything. Removed remaining JS. Removed icon font with SVG images.
    - Added translations for everything in Swedish and English. Closes #101
    - Removed remaining JavaScript. For now the theme is all HTML/CSS
    - Removed copyright statement from footer
    - Fixed display of colors in the header.
    - New way of handling animation order controlled from the markup
    - Removed compass-reset and added normalize

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,20 +2,4 @@
 /* exported Saga */
 
 let Saga = (() => {
-    let nav = document.querySelector('#dropdown-menu--toggle');
-    let toggleMenu = (selector) => {
-        let element = document.querySelector(selector);
-        element.setAttribute('data-state', element.getAttribute('data-state') === 'closed' ? 'open' : 'closed');
-    };
-
-    nav.onclick = (e) => {
-        toggleMenu('#dropdown-menu', 'closed', 'open');
-        e.preventDefault();
-        e.stopPropagation();
-    };
-
-    document.onclick = () => {
-        let element = document.querySelector('#dropdown-menu');
-        element.setAttribute('data-state', 'closed');
-    };
 })();
