846d22f8bdee9ece6d14492c9201dfc64cb7b686
Gustav Lindqvist <gustav@lindqvist.io>
2020-02-29 18:23:01 +0100

    Refactored SCSS according to Inverted Triangle CSS. Made fonts, margins, paddings, sizes, colors into variables. Removed px with rem everywhere. Changed design of lists.
     Closes #116, #111, #24

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 /* eslint-env node */
+/* global exports */
 const { src, dest, parallel, watch } = require('gulp');
 const browsersync = require('browser-sync').create();
 const reload = browsersync.reload;
-const sass = require('gulp-sass');
+const sass = require('gulp-dart-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const minifycss = require('gulp-clean-css');
@@ -14,6 +15,29 @@ const plumber = require('gulp-plumber');
 function css() {
     'use strict';
     return src('sass/main.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init({largeFile: true}))
+        .pipe(rename({
+            basename: 'style'
+        }))
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(autoprefixer({ grid: true }))
+        .pipe(csscomb())
+        .pipe(sourcemaps.write())
+        .pipe(dest('assets/css/'))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write())
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(dest('assets/css/'))
+        .pipe(reload({stream: true}));
+}
+
+function newCss() {
+    'use strict';
+    return src('sass/inverted-triangle.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init({largeFile: true}))
         .pipe(rename({
@@ -50,6 +74,24 @@ function serve() {
     });
 }
 
+function newServe() {
+    'use strict';
+    browsersync.init({
+        logPrefix: 'Saga for Ghost',
+        port: 3000
+    });
+
+    watch('sass/**/*.scss', newCss);
+    watch(['./*.hbs', './partials/*.hbs'], () => {
+        reload();
+    });
+    watch('assets/**/*.js', () => {
+        reload();
+    });
+}
+
 exports.css = css;
+exports.newcss = newCss;
 exports.server = serve;
 exports.default = parallel(css, serve);
+exports.newdefault = parallel(newCss, newServe);
