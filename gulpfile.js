1a4c11defd2dbf148194bd69dca6158f34807fca
Gustav Lindqvist <hej@gustavlindqvist.se>
2020-09-22 20:13:32 +0200

    Renamed sass folders. Started conversion to BEMIT.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 /* eslint-env node */
-/* global exports */
-const { src, dest, parallel, watch } = require('gulp');
+const project = require('./package.json');
+const { src, dest, series, watch } = require('gulp');
 const browsersync = require('browser-sync').create();
-const reload = browsersync.reload;
-const sass = require('gulp-dart-sass');
+const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const minifycss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sassGlob = require('gulp-sass-glob');
-const csscomb = require('gulp-csscomb');
 const plumber = require('gulp-plumber');
+const zip = require('gulp-zip')
 
-function css() {
+function reloadBrowsers(done) {
+    browsersync.reload();
+    done()
+}
+
+function compileCSS() {
     'use strict';
     return src('sass/main.scss')
         .pipe(plumber())
@@ -23,7 +27,6 @@ function css() {
         .pipe(sassGlob())
         .pipe(sass())
         .pipe(autoprefixer({ grid: true }))
-        .pipe(csscomb())
         .pipe(sourcemaps.write())
         .pipe(dest('assets/css/'))
         .pipe(minifycss())
@@ -32,30 +35,7 @@ function css() {
             extname: '.min.css'
         }))
         .pipe(dest('assets/css/'))
-        .pipe(reload({stream: true}));
-}
-
-function newCss() {
-    'use strict';
-    return src('sass/inverted-triangle.scss')
-        .pipe(plumber())
-        .pipe(sourcemaps.init({largeFile: true}))
-        .pipe(rename({
-            basename: 'style'
-        }))
-        .pipe(sassGlob())
-        .pipe(sass())
-        .pipe(autoprefixer({ grid: true }))
-        .pipe(csscomb())
-        .pipe(sourcemaps.write())
-        .pipe(dest('assets/css/'))
-        .pipe(minifycss())
-        .pipe(sourcemaps.write())
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(dest('assets/css/'))
-        .pipe(reload({stream: true}));
+        .pipe(browsersync.reload({stream: true}));
 }
 
 function serve() {
@@ -65,33 +45,24 @@ function serve() {
         port: 3000
     });
 
-    watch('sass/**/*.scss', css);
-    watch(['./*.hbs', './partials/*.hbs'], () => {
-        reload();
-    });
-    watch('assets/**/*.js', () => {
-        reload();
-    });
+    watch('sass/**/*.scss', compileCSS);
+    watch(['./*.hbs', './partials/*.hbs'], reloadBrowsers);
+    watch('assets/**/*.js', reloadBrowsers);
 }
 
-function newServe() {
-    'use strict';
-    browsersync.init({
-        logPrefix: 'Saga for Ghost',
-        port: 3000
-    });
-
-    watch('sass/**/*.scss', newCss);
-    watch(['./*.hbs', './partials/*.hbs'], () => {
-        reload();
-    });
-    watch('assets/**/*.js', () => {
-        reload();
-    });
+function createPackage() {
+    return src([
+        './**',
+        '!node_modules/**',
+        '!.git/**',
+        '!.DS_Store',
+        '!.gitignore'
+    ], { dot: true })
+        .pipe(zip('Saga-' + project.version + '.zip'))
+        .pipe(dest('./'));
 }
 
-exports.css = css;
-exports.newcss = newCss;
-exports.server = serve;
-exports.default = parallel(css, serve);
-exports.newdefault = parallel(newCss, newServe);
+exports.default = series(compileCSS, serve);
+exports.compileCSS = compileCSS;
+exports.serve = serve;
+exports.createPackage = createPackage
