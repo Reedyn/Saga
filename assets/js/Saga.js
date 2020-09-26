d38b45517dbf62172d847c4cc3bdd46a5816b964
Gustav Lindqvist <hej@gustavlindqvist.se>
2020-09-26 18:16:26 +0200

    Added subscriber feature.
    Closes #106

--- assets/js/Saga.js
+++ assets/js/Saga.js
@@ -2,7 +2,18 @@
 
 let Saga = (() => {
     'use strict';
-    let fitVids = () => {
+
+    let _getParameterByName = (name, url) => {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+    
+    let _fitVids = () => {
         const players = [
             'iframe[src*="youtube.com"]',
             'iframe[src*="soundcloud.com"]',
@@ -31,9 +42,9 @@ let Saga = (() => {
         }
     };
 
-    let ghostGallery = () => {
+    let _gallery = () => {
         const images = document.querySelectorAll('.kg-gallery-image img');
-        images.forEach(function (image) {
+        images.forEach((image) => {
             let container = image.closest('.kg-gallery-image');
             let width = image.attributes.width.value;
             let height = image.attributes.height.value;
@@ -42,6 +53,76 @@ let Saga = (() => {
         });
     };
 
-    fitVids();
-    ghostGallery();
+    let _subscribeFeature = () => {
+        // Give the parameter a variable name
+        const body = document.querySelector('body');
+        const subscribeCloseButton = document.querySelector('.subscribe-close-button');
+        const subscribeSuccessMessage = document.querySelector('.subscribe-success-message');
+        const subscribeModal = document.querySelector('.subscribe-modal');
+        const subscribeModalClose = document.querySelectorAll('.subscribe-modal__close');
+        const subscribeInputEmail = document.querySelector('.subscribe-form__input.-email');
+        const subscribeButtons = document.querySelectorAll('a[href="#subscribe"]');
+        const notSubscribeModal = document.querySelectorAll('body > *:not(.subscribe-modal)');
+
+        let _oldFocusElement = undefined;
+
+        const _showNotSubscribeModal = () => {
+            notSubscribeModal.forEach((element) => {
+                element.removeAttribute('aria-hidden');
+            })
+        }
+        const _hideNotSubscribeModal = () => {
+            notSubscribeModal.forEach((element) => {
+                element.setAttribute('aria-hidden', 'true');
+            })
+        }
+        const _showSubscribeModal = () => {
+            _hideNotSubscribeModal()
+            subscribeModal.classList.remove('hidden');
+            subscribeInputEmail.value = '';
+        }
+        const _hideSubscribeModal = () => {
+            _showNotSubscribeModal();
+            subscribeModal.classList.add('hidden');
+            _oldFocusElement.focus();
+        }
+
+        if (_getParameterByName('action') === 'subscribe') {
+            body.classList.add("subscribe-success");
+        }
+
+        subscribeCloseButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            subscribeSuccessMessage.classList.add('close');
+        });
+
+        subscribeModalClose.forEach((element) => {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+                _hideSubscribeModal()
+            });
+        });
+
+        window.addEventListener('keyup', (event) => {
+            if (event.key === 'Escape') {
+                _hideSubscribeModal()
+            }
+        })
+
+        if (window.location.hash === '#subscribe') {
+            _showSubscribeModal()
+        }
+
+        subscribeButtons.forEach((element) => {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+                _oldFocusElement = element;
+                _showSubscribeModal()
+            });
+        });
+    }
+
+    _fitVids();
+    _gallery();
+    _subscribeFeature();
 })();
